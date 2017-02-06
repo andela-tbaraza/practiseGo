2eddc204da75217284a649a3ef2f87e8133f66d5
Tonida Baraza <tonida.baraza@andela.com>
2017-02-06 21:24:57 +0300

    variables

--- hello-world.go
+++ hello-world.go
@@ -3,8 +3,19 @@ package main
 import (
   "fmt"
   "runtime"
+  "reflect"
   )
-
+var (
+  name, location string // global variable
+  number int32 // package level
+  fly = 0.32
+)
 func main() {
+  house := 20000 // shorthand declare and initialize only works within a function
+  name = "Today"
   fmt.Println("Hello from", runtime.GOOS)
+  fmt.Println("Hello", name, "and is  of type", reflect.TypeOf(name))
+  fmt.Println(number)
+  fmt.Println(location)
+  fmt.Println("fly is of type", reflect.TypeOf(fly))
 }
