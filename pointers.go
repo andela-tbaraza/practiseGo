3496db378e956edabf23d77eba650c7f06567d3f
Tonida Baraza <tonida.baraza@andela.com>
2017-02-07 23:24:46 +0300

    pointers

---
+++ pointers.go
@@ -0,0 +1,22 @@
+package main
+
+import (
+  "fmt"
+)
+
+func main () {
+  module := 23
+  ptr := &module
+  lang := "Javascript"
+
+  fmt.Println("\nThe memory address of module is", ptr)
+  fmt.Println("\nThe value of module is", *ptr)
+  changeLang(&lang)
+  fmt.Println("\nWe started programming in", lang) // lang variable is unchanged
+}
+
+func changeLang(lang *string) string {
+  *lang = "Go" // notice we don't use shorthand since we are not declaring a new variable
+  fmt.Println("\nnWe are now programming in", *lang)
+  return *lang
+}
