d866b7b3b135d96bed83d13cdd97ef0f411f2204
Tonida Baraza <tonida.baraza@andela.com>
2017-02-08 09:25:47 +0300

    constants

---
+++ constants.go
@@ -0,0 +1,5 @@
+// Constants are like variabes onlly thar they are immutable
+// You can't use short hand toinitialize constants
+// You should use const key word to initialize constants
+
+const speed = 100
