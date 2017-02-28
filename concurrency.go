002cfb11db501caa2ef17fa6dd2f366f1cbfd54b
Tonida Baraza <tonida.baraza@andela.com>
2017-02-28 16:21:48 +0300

    concurrency

---
+++ concurrency.go
@@ -0,0 +1,22 @@
+package main
+
+import (
+    "fmt"
+)
+
+func main () {
+    // concurrency vs paralleslism
+    // Mike van sickle - concurrent programming in Go
+    // concurrency is abput independent executing process
+    // Robe pike http://blog.golang.org/concurrency-is-not-paralleslism
+    // Go routines are scheduled by go run time
+    // Gor routine just layers on tops of threads
+    // Why??
+    // - Lightweight
+    // go manages all go routines
+    // Less switching - switching multiple routines against a single thread
+    // Faster start up times
+    // safe communication - channels
+    // Go concurrency model - CSP Communicating Sequential Processes - Actor model
+
+}
